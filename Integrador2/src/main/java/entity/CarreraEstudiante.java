86e4efe6cae3a72712d358f71df5b210179f6e0a
unknown <Ricardo Gentil>
2022-10-22 10:40:52 -0300

    .gitignore

--- Integrador2/src/main/java/entity/CarreraEstudiante.java
+++ Integrador2/src/main/java/entity/CarreraEstudiante.java
@@ -9,26 +9,26 @@ import javax.persistence.MapsId;
 
 @Entity
 public class CarreraEstudiante {
-	
+
 	@EmbeddedId
 	private CarreraEstudianteId id;
-	
-	@ManyToOne
-    @MapsId("studentId")
-	@JoinColumn(name = "student_id")
-    private Estudiante estudiante;
-    
-	@ManyToOne
-    @MapsId("careerId")
-    @JoinColumn(name = "career_id")
-    private Carrera carrera;
-    
-	@Column 
-    private int antiguedad;
-	
-	@Column
-    private Boolean graduado;
-    
 
+	@ManyToOne
+	@MapsId("studentId")
+	@JoinColumn(name = "student_id")
+	private Estudiante estudiante;
+
+	@ManyToOne
+	@MapsId("careerId")
+	@JoinColumn(name = "career_id")
+	private Carrera carrera;
+
+	@Column
+	private int antiguedad;
+
+	@Column
+	private Boolean graduado;
+	
+	
 
 }
--- Integrador2/src/main/java/entity/CarreraEstudianteId.java
+++ Integrador2/src/main/java/entity/CarreraEstudianteId.java
@@ -3,13 +3,32 @@ package main.java.entity;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
-@Embeddable //Se indica que la clase puede ser embebida por otra(? saco pk compuesto
+@Embeddable // Se indica que la clase puede ser embebida por otra(? saco pk compuesto
 public class CarreraEstudianteId {
 
 	@Column(name = "estudiante_id")
-    private Long estudianteId;
-	
-    @Column(name = "carrear_id")
-    private Long carreraId;
- 
+	private Long estudianteId;
+
+	@Column(name = "carrear_id")
+	private Long carreraId;
+
+	public CarreraEstudianteId(Long estudianteId, Long carreraId) {
+		super();
+		this.estudianteId = estudianteId;
+		this.carreraId = carreraId;
+	}
+
+	public CarreraEstudianteId() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getEstudianteId() {
+		return estudianteId;
+	}
+
+	public Long getCarreraId() {
+		return carreraId;
+	}
+
 }
